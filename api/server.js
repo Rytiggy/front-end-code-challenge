a0ed5d48d8dc03321eb72b801eef2234b7935d45
Ryan Mason <ryanmasonjar@gmail.com>
2019-06-17 16:03:48 -0400

    Add Cors to server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      locations = require('./routes/locations')
+      locations = require('./routes/locations'),
+      cors = require('cors')
+
 
 const app = express()
+
+app.use(cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
