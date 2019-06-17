2f4066a7e52c6f53108633c3db4ca67798ccbaa4
Ryan Mason <ryanmasonjar@gmail.com>
2019-06-17 15:26:10 -0400

    Create Quasar app

---
+++ client/src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    {{info}}
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "PageIndex",
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    this.$axios
+      .get("localhost:3000/api/locations/")
+      .then(response => (this.info = response));
+  }
+};
+</script>
